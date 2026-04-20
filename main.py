c8f74c46a1a07f2f93a7c14501d65ecc9c8b7d61
masarclaudia <masarclaudia@gmail.com>
2026-04-20 13:01:42 +0200

    2

--- main.py
+++ main.py
@@ -1,2 +1,33 @@
-import random
-def random_numbers
+from sorting import random_numbers, selection_sort, bubble_sort
+
+
+def main():
+    numbers = [5, 1, 4, 2, 8]
+
+    print("Pôvodný zoznam:\n", numbers)
+    print()
+
+    print("Selection sort:")
+    print(selection_sort(numbers))
+    print()
+
+    print("Bubble sort:")
+    print(bubble_sort(numbers))
+
+    random_list = random_numbers(20)
+    print()
+
+    print("Náhodný zoznam:")
+    print(random_list)
+    print()
+
+    print("Selection sort náhodného zoznamu:")
+    print(selection_sort(random_list))
+    print()
+
+    print("Bubble sort náhodného zoznamu:")
+    print(bubble_sort(random_list))
+
+
+if __name__ == "__main__":
+    main()
--- sorting.py
+++ sorting.py
@@ -1,5 +1,5 @@
 import random
-
+import matplotlib.pyplot as plt
 def random_numbers(count, low=0, high=100):
     return [random.randint(low, high) for _ in range(count)]
 
@@ -41,18 +41,57 @@ def selection_sort(numbers):
 
 
 def bubble_sort(numbers):
+    #numbers = [5, 4, 1, 2, 8]
+    plt.ion()
+    plt.show()
+    numbers = [5, 4, 1, 2, 8]
+    numbers = numbers.copy()
+    number = len(numbers)
+
+    for i in range(number):
+        for j in range(0, number - i - 1):
+            if numbers[j] > numbers[j + 1]:
+                temp = numbers[j]
+                numbers[j] = numbers[j + 1]
+                numbers[j + 1] = temp
+            index_highlight1 = j
+            index_highlight2 = j + 1
+            colors = ["steelblue"] * len(numbers)
+            colors[index_highlight1] = "tomato"
+            colors[index_highlight2] = "tomato"
+            plt.clf()
+            plt.bar(range(len(numbers)), number, color=colors)
+            plt.title("Bubble Sort")
+            plt.pause(0.1)
+    plt.ioff()
+    plt.show()
+    # print(numbers)
     return numbers
 
 
+# plt.ion()
+# plt.show()
+# numbers = [5, 4, 1, 2, 8]
+# numbers = numbers.copy()
+# number = len(numbers)
+#
+# for i in range(number):
+#     for j in range(0, number - i - 1):
+#         if numbers[j] > numbers[j + 1]:
+#             temp = numbers[j]
+#             numbers[j] = numbers[j + 1]
+#             numbers[j + 1] = temp
+#         index_highlight1 = j
+#         index_highlight2 = j + 1
+#         colors = ["steelblue"] * len(numbers)
+#         colors[index_highlight1] = "tomato"
+#         colors[index_highlight2] = "tomato"
+#         plt.clf()
+#         plt.bar(range(len(numbers)), number, color=colors)
+#         plt.title("Bubble Sort")
+#         plt.pause(0.1)
+# plt.ioff()
+# plt.show()
+# print(numbers)
 
-numbers = [5, 4, 1, 2, 8]
-numbers = numbers.copy()
-number = len(numbers)
 
-for i in range(number):
-    for j in range(0, number - i - 1):
-        if numbers[j] > numbers[j + 1]:
-            temp = numbers[j]
-            numbers[j] = numbers[j + 1]
-            numbers[j + 1] = temp
-print(numbers)
