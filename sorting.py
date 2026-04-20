247d295481f0a22b571ad524deaef44332801cc1
masarclaudia <masarclaudia@gmail.com>
2026-04-20 12:41:06 +0200

    Řešení cvičení 11_1

--- sorting.py
+++ sorting.py
@@ -1,6 +1,58 @@
-def bubble_sort():
+import random
 
-def selection_sort():
+def random_numbers(count, low=0, high=100):
+    return [random.randint(low, high) for _ in range(count)]
 
-def random_numbers():
 
+
+# values = random_numbers(10)  # 10 čísel v rozsahu 0–100
+# print(values)  # např. [42, 7, 91, 15, 63, 8, 57, 73, 2, 100]
+#
+# small = random_numbers(5, low=0, high=20)  # 5 čísel v rozsahu 0–20
+
+
+def selection_sort(numbers):
+    number = len(numbers)
+
+    for i in range(number):
+        min_index = i
+
+        for j in range(i + 1, number):
+            if numbers[j] < numbers[min_index]:
+               min_index = j
+        temp = numbers[i]
+        numbers[i] = numbers[min_index]
+        numbers[min_index] = temp
+    return numbers
+
+
+# numbers = [5, 4, 1, 2, 8]
+# number = len(numbers)
+# for i in range(number):
+#     min_index = i
+#
+#     for j in range(i + 1, number):
+#         if numbers[j] < numbers[min_index]:
+#            min_index = j
+#     temp = numbers[i]
+#     numbers[i] = numbers[min_index]
+#     numbers[min_index] = temp
+# print(numbers)
+
+
+def bubble_sort(numbers):
+    return numbers
+
+
+
+numbers = [5, 4, 1, 2, 8]
+numbers = numbers.copy()
+number = len(numbers)
+
+for i in range(number):
+    for j in range(0, number - i - 1):
+        if numbers[j] > numbers[j + 1]:
+            temp = numbers[j]
+            numbers[j] = numbers[j + 1]
+            numbers[j + 1] = temp
+print(numbers)
